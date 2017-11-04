c6965a5e2aa5aece289431e669b18e045dbd5e12
Sakthi <sakthi.rjp@gmail.com>
2017-11-03 21:53:05 -0400

    Script to cache matix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##makeCacheMatrix generates a list with 3 functions to get the matix value,set and display inverse value
 
+makeCacheMatrix <- function(x = matrix()) {
+    i <- NULL
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
+## CacheSolve, first fetches value of i(Matrix inverse) and checks if a value os store ot not.
+##If a value is loaded, function retunrs the  pre-loaded matix inverse else calculates in the inverse, 
+## set it i and diplays the inverse
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+          message("getting cached data")
+          return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i        
 }
